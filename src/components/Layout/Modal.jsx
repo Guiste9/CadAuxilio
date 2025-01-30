f5a2618966565b738369e52aa5d6f51d92e1910c
Guiste9 <pessoa.marinho06@aluno.ifce.edu.br>
2025-01-30 17:55:08 -0300

    PopUpComponent/feature

---
+++ src/components/Layout/Modal.jsx
@@ -0,0 +1,33 @@
+import Button from "./Button";
+import React from "react";
+import styles from "./Modal.module.css";
+
+function Modal({ closeModal }) {
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContainer}>
+        
+       
+        <div className={styles.titleCloseBtn}>
+          <button onClick={() => closeModal(false)}>×</button>
+        </div>
+
+        <div className={styles.title}>
+          <h1>Olá, meu amigo!</h1>
+        </div>
+
+        <div className={styles.body}>
+          <p>A próxima página é maneira!</p>
+        </div>
+
+        <div className={styles.footer}>
+          <button className={styles.cancelBtn} onClick={() => closeModal(false)}>Cancelar</button>
+          <button className={styles.proceedBtn}>Prosseguir</button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,18 +5,21 @@ import { useState } from 'react'
 import Button from '../Layout/Button'
 import Login from '../Login/Login'
 import Cadastrador from '../Login/Cadastrador'
+import Modal from '../Layout/Modal'
 
 function Home(){
-    const [open,setOpen] = useState(false)
+    const [openModal,setOpenModal] = useState(false)
     return (
         <section className={styles.home_container}>
             <h1>bem-vindo ao <span>CadAuxilio</span></h1>
             <img src={coracao} alt="CadAuxilio" style={styles.img} />
-            <div className='styles.button_container'>
-                <Button text = "Cadastrar-se"  to={<Cadastrador/>} onClick={() => setOpen(!open)}/>
-                <Button text = "Login"  to={<Login/>} onClick={() => setOpen(!open)}/>
+            <div className={styles.button_container}>
+                {/*<Button text = "Cadastrar-se"  to={<Cadastrador/>} onClick={() => setOpenModal(!openModal)}/>
+                <Button text = "Login"  to={<Login/>} onClick={() => setOpenModal(!openModal)}/>*/}
+                <button className='openmodalbtn' onClick={() => {setOpenModal(true)}}>open</button>
+                {openModal && <Modal closeModal={setOpenModal}/>}
             </div>
-            <Popup isOpen={open}/>
+            
             <h2>Sobre</h2>
             <p>Somos a Igreja Evangélica Rio da Vida, estamos atuando no centro de Maracanaú a sete anos e desde então levamos o evangelho de Cristo para todos, promovendo ações evangelísticas junto de ajuda para os mais carentes da comunidade local.</p>
             <p>Em nossa, localizada na avenida Padre José Holanda do Vale em Maracanaú, existe um projeto social que procura auxiliar as crianças carentes das comunidades mais próximas. Visando isso o CadAuxilios foi criado para suprir esse problema, utilizando ferramentas de frequência para as famílias das crianças receberem as cestas basicas ofertadas pela igreja.</p>
